7cb4526ed8affdb58f80a3d0a25ea7eee9a38763
elefevre <eric@ericlefevre.net>
2018-12-11 22:35:56 +0100

    Add first 4 days

---
+++ src/net/ericlefevre/Day2IDScans.java
@@ -0,0 +1,55 @@
+package net.ericlefevre;
+
+public class Day2IDScans {
+    static String[] ids = new String[] {"prtkqyluibmtcwqaezjmhgfndx","prtkqylusbsmcwvaezjmhgfndt","prgkqyluibsocwvamzjmhgkndx","prjkqyluibsocwvahzjmhgfnsx","prtkqylcibsocwvzezjohgfndx","prtkqyluiksocwziezjmhgfndx","prikqyluiksocwvaezjmkgfndx","prtkgyluibsocwvwezjehgfndx","prtkqyluiysocwvaezjghxfndx","prtkqwluibsoxwvaezjmhgfhdx","prtkqylgibsocwvabzjmhzfndx","prtknyltibnocwvaezjmhgfndx","prdkqyluibrocwvaezjmhgnndx","prtwqyluibsoctvcezjmhgfndx","mrtkqyluibgocwvakzjmhgfndx","prtkqaouibsocwvaezjmhwfndx","prtkqyluihjocwvaezjmhgfpdx","prtkqyluikfxcwvaezjmhgfndx","prtkqybuixsocwvaczjmhgfndx","pvtkayluibsocwxaezjmhgfndx","grtkqgluibsocdvaezjmhgfndx","prlkqyluibsochvaezjmhgzndx","prtkqylxibsocmvaezjmhgfkdx","prtkqyluibsqctvaezjmpgfndx","putkqyluibsocqvaezjmhgfndw","prtjqyluibsiclvaezjmhgfndx","prtkqylvpvsocwvaezjmhgfndx","prnkqyluibsocwvaezjmhefsdx","prtktyluibsocwvaezjkhgrndx","prtkqyluibcovwvaezjthgfndx","prtkqcluibiocwvaezjmhggndx","prtkqyluihsocwveezjmhgfydx","prtklyluibsocwqaszjmhgfndx","prtkqyluibsocwvaezjmfznndx","prtkjyluijsocwvaeejmhgfndx","prtkqtluibsonwvaexjmhgfndx","prtkqyluinsocwbaezjmjgfndx","prtkqyluibslckvaezjmhgyndx","prtkqyluibsodwlpezjmhgfndx","prtkquluibsfcwvaezjhhgfndx","prtkqyluhbsocweaezsmhgfndx","prrkqyluinsocxvaezjmhgfndx","prtkqyluibsoswvaezjmhgyqdx","prtkqbluibdocwvlezjmhgfndx","prtkqyfuibsocpvaezjmhgfnwx","prtkqlluibsqjwvaezjmhgfndx","prtkqyluibrocwvaehjmjgfndx","prtkqyluibsoowvaezgmhgendx","wrtjqyluibsocwvaezfmhgfndx","prtvqyluhbsocwvaezjmtgfndx","prtkqyllibspcwvaezjmkgfndx","pqtzqyeuibsocwvaezjmhgfndx","prtkqyluibsolpvaezjmegfndx","przkayguibsocwvaezjmhgfndx","prtkqyluidsocwvaezjmyufndx","prtuqyluibsocwvaezjmfgfnkx","prtkqwluibsrcwvaezjchgfndx","prtkqyluibsotwhaozjmhgfndx","erwkqylhibsocwvaezjmhgfndx","prtkqyluibsocwvgezjmkgfedx","prskqyluiesocwvaezjmggfndx","prtkqylmitsocwvaezjmhgfnox","prtkqyluinnocwvaezjmhgfkdx","prtktyluibsokwvaezjmhgfcdx","prtkqyluibsomwvakvjmhgfndx","prtkqyltibloawvaezjmhgfndx","prtkqyluibxocwvaezgmhgqndx","prtkqyluibskcmvaezjmhgfngx","artkqylubbsotwvaezjmhgfndx","prtkqyluibzocwvhezjmhgfnbx","prskqkluibsocwvaezjmhgfjdx","prtkqyluibwocwvaezjkhglndx","prukqyluissocwvzezjmhgfndx","puhkqyluibsocwvaezjmhgfsdx","qrtkqyluibsocwvaeujmhgfndd","prtkqyluibsoctvaezjmagfnda","prtkquluibsocwkaezjmhgfqdx","prtkqyluubswcwvaezjmhvfndx","prfkqyluibsocwvaemrmhgfndx","pmtkqyluibpocwvaezjmhggndx","prtkqvluibiocwvaezjqhgfndx","prtkgypuibsocwvaezcmhgfndx","prtpqyquibsovwvaezjmhgfndx","prtwqyluiasocwvaexjmhgfndx","mrtzqyluibbocwvaezjmhgfndx","prtkqyluibsocwmaegwmhgfndx","prtkqyluibvncwvaqzjmhgfndx","prtkqyluiusocwvaezjmhmfbgx","prtkqyljibvocwvaezjehgfndx","prtkqyloibsopavaezjmhgfndx","prckqyakibsocwvaezjmhgfndx","prtkqyluibsdcwvaezjmngfddx","prekqylupbsocwvaezemhgfndx","hrtkqyluibhocwvaezjmhgfnde","prmkqyluibsocwvaezzfhgfndx","prtkqyluiccfcwvaezjmhgfndx","pdtkqyluxbsocwvaezjmhgendx","prokqyluibsocwvuezjmsgfndx","prtkqyluibsacwvaezjyhgfndv","prtkqmluibsocavaezjmhgfndc","prtkqyluibsocwvmezjmhgtnqx","prtkqytuibiocyvaezjmhgfndx","pktkqyiuibsocwvwezjmhgfndx","grtrqyluibsocwvaezjmhgfbdx","prtkqylsibjocwvaezjmhgfnyx","prtkqyhutbsocwvaexjmhgfndx","prtknyluibsocmvaezumhgfndx","prtkwyluibsocwvahzjmhgpndx","prtkqywuibsolhvaezjmhgfndx","prtkcyluibsoccvaezjthgfndx","prtkqyrdibsocwvaezjbhgfndx","prtkqyhuqbsocwvaezjmhgfxdx","pytkqyluibsocwvagzjmhgfndv","prtkqyliibsocwvaexwmhgfndx","prtkqyluibshcwvaeljphgfndx","prtkqyluibsocwvaerjzhbfndx","prtkqyduibsocwvaezvmhgfnzx","drtkqylhibsocwvaezjmhmfndx","prtkqyluibsocwvaezamfvfndx","brtkqyluqbsocwvaezjmhgpndx","prtkqyiuibsocwvuezjmhgfngx","urtkqyluibsocqvaeljmhgfndx","prtkqyluikaocwvaezjmhgfjdx","prqkqzouibsocwvaezjmhgfndx","prtkqyluibsocxvaezjmhgfnxv","prlkqyluibsoxwvaeijmhgfndx","prthuyluibsocwvaezjmhgfnhx","potkqyluizsocwvaezjmhifndx","fstkqyduibsocwvaezjmhgfndx","prtkqxluibsocwvaezjmhgffdm","prtkqylpibsozwvaezmmhgfndx","prxkqylbibsocwvaezjphgfndx","srtkqyluibsicnvaezjmhgfndx","prtktyluibsocwvaezjvhgfnax","pctkqyluxbsocwvaezwmhgfndx","prtkqylusbsoclvaezsmhgfndx","pwtkqyluibsocrvaezjmggfndx","prtkqyluibswcwraezjmhgfndd","prtkqyluibtocwiaezjmhgfnax","prtuqyluibsocwvajzjmngfndx","pwtkqyluibsocwvaerjmogfndx","petkqexuibsocwvaezjmhgfndx","pztkqyluibsocwvaerqmhgfndx","prtkqyluobsocwvaezjmapfndx","prtkqyluiinocwvaeljmhgfndx","prtkqyluibsoowvxezjmhgfnnx","lrtkqyluibsocwvfezjmhgfndc","prtkqyluibokcwvahzjmhgfndx","prtkqmlufbsocwvaegjmhgfndx","prtkqylribsocwvanzjmhgfnda","prtkqyluibspxwvaezkmhgfndx","prtiqyluibsbcwvaezjmhgfntx","prikqzluinsocwvaezjmhgfndx","prtkqnldibsocwvaezjmhxfndx","prtkqyluixsocsvaezjmhwfndx","hrtkqyluibsocwvaezjhhgfodx","prtkqyluibsrcwvaezjmhpfwdx","prtkqyluibsocwyaezjmhgffdk","prtkqyluidsocwvalmjmhgfndx","prukquluabsocwvaezjmhgfndx","prckqyluinsmcwvaezjmhgfndx","prbkqymuibsocwvaezjmhgfndc","prtkfylaibsocwvaezjmkgfndx","zrtkqyluibsocwvrbzjmhgfndx","crtkqyluibsocwvaejjmkgfndx","prttqyluibsocyvaezymhgfndx","prtkqylugbsocwvaezjxhgfmdx","prtkqyluibsocwdlezjmhgfnbx","prtkqjluibsocwvaozjhhgfndx","prtcjyluibsocwbaezjmhgfndx","rrtkqyluiblocwvaezjmhgundx","prtkkyluibsocwfaezjmhgfnyx","prtkqyuuibsocwvaezjmhgfogx","prtkyyluvbsocwvaezjmhgfnox","prpkqyluibyocwvaezjmhggndx","pdtkqyluibdocwvaezjmhgfndy","prtklysuibsocwvaezjmhgfnwx","prtkqyluabsouwvaekjmhgfndx","phtkqyluibsocwvaezjmhgfnxt","prtkqyxuibsocwvaezjmhpfnqx","prtkqyluibsodwsaezdmhgfndx","prtkbyluibsohwvaezjmhgfndr","xrtkqylhibsocwvtezjmhgfndx","prtkqyluvysocwvaezbmhgfndx","prtkqieuibsocwvaeojmhgfndx","pctkqyluibsocwvanzjmhgfnux","vrtkqyluibsozwvaezjmhgandx","prtkqyluiusocwvaezjmhmfngx","prbkqyluibsockvaxzjmhgfndx","prtkqyluibsonwvaczjmhgfndi","prtkqyluiblocwvaezjmhgfnau","prtkqyluibsocwvafzuchgfndx","prdkqyluiysocwvaezjmhgfnax","prnkqyouibsocwvaezjmhgfndq","mrtkqgluibsocwvpezjmhgfndx","pvtkqyluibsocwvaczjmhgnndx","trtkqwluibsohwvaezjmhgfndx","prmkqyluibsofwvaezjmhgfrdx","prtyqyluibpdcwvaezjmhgfndx","ertkqylulbsocwvaezjmhgfnax","prtkqyluibsacwvaeijmhgfndf","prtkqyluibyocwvapzjmhgpndx","potkqyluibgocwvaezjmhzfndx","prtkqyluibsocwyaezxmhgfnpx","prtkqkjuibsncwvaezjmhgfndx","prtqqyluibsocwlaezjmhgkndx","prtkxyluibnocwvaezjmhgkndx","prtkqyluiosocwvapzjmxgfndx","prtkqylumbsocwvyezimhgfndx","prukqyluibsocwvyezjmhgindx","prtkqylbibstcwvaezjxhgfndx","pctkqyuuibsocwvaezjuhgfndx","vrtkqyluibsocwvaezjmhgfnll","urtkqyluibsopwvaezjphgfndx","prtkceluibsocwvaepjmhgfndx","prwkxyluibsocwvaezjmhgfnzx","prtkqyluitsocwvaezqzhgfndx","prtkqkauibsorwvaezjmhgfndx","prtkqyluibsocwvaezfmftfndx","prtkiybuibsocwvaezjkhgfndx","prtkzyluibsocwgaezjmvgfndx","prtkqyluibsocwvaezjmhgqnxg","prtkqyluimsocwvauzjwhgfndx","prtkqyluibsacwgaezjmhgfndd","pwtkuyluibsccwvaezjmhgfndx","prtkqyluibsoawvaezjmvgfnlx","prtkqyluabsocwwaezjmhgftdx","patkqylnibsocwvaezjmhgfnox","prtkqyluibsocwlaxzkmhgfndx","pbtkqpluibsfcwvaezjmhgfndx","prtkqyluibsoywsaezjmhgxndx","prtkqyluibfocwvaezjyhgfhdx","pltbqylcibsocwvaezjmhgfndx","prtkdyluiisocwvvezjmhgfndx","prtkqkxuibsokwvaezjmhgfndx","prtkqyluibsoawvaezzmhgfndm","petkqyluibsgcwvaezjmhgfndu","prtkqyluibsoyxvaezjmlgfndx","prtkqyluibxocwvaezgmhnfndx","prtkikluibsocwvwezjmhgfndx","prbkqyluibsocwvaezjhhgfnux","prtkqylufbsxcwvaezjmhgfnfx","prtkqyluibsdcdvaezjmhgxndx","potkiyluibsocwvaezjmhkfndx","prtkqyluiosocsvhezjmhgfndx","prtkqyluibsocqbaezomhgfndx","prtihyluibsocwvaeujmhgfndx","prtuquruibsocwvaezjmhgfndx","prtkqyloibsocwvaeztmhifndx","ertuqyluibsocwvaeajmhgfndx"};
+
+    public static void main(String[] args) {
+        int countTwos = 0;
+        int countThrees = 0;
+        for (String id : ids) {
+            countTwos += countFor(id, 2);
+            countThrees += countFor(id, 3);
+        }
+        System.out.println(countTwos * countThrees);
+
+
+        for (int i = 0 ; i < ids.length ; i++) {
+            String first = ids[i];
+
+            for (int j = i+1 ; j < ids.length ; j++) {
+                String second = ids[j];
+//                System.out.print(first + " ("+i+") versus " + second+ " ("+j+")");
+                int differences = 0;
+
+                for (int k = 0 ; k < first.length(); k++) {
+                    if (first.charAt(k) != second.charAt(k)) {
+                        differences++;
+                        if (differences> 1) {
+  //                          System.out.println(": at least 2 differences");
+                            break;
+                        }
+                    }
+                }
+                if (differences==1) {
+                    System.out.println(first + " is close to " + second);
+                }
+
+            }
+        }
+
+    }
+
+    static int countFor(String id, int occurrences) {
+        int[] letters = new int[26];
+        for (char letter : id.toCharArray()) {
+            letters[letter - 'a']++;
+        }
+
+        for (int occurence:letters) {
+            if (occurence == occurrences) {
+                return 1;
+            }
+        }
+        return 0;
+    }
+}
